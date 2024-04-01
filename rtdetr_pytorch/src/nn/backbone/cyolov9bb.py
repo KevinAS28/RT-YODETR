1fc9094a1044134a1d9d62072b7e0041e8e5f08c
Kevin Agusto <kevinagusto28@gmail.com>
2024-04-01 20:22:45 +0700

    added cyolov9bb

---
+++ rtdetr_pytorch/src/nn/backbone/cyolov9bb.py
@@ -0,0 +1,46 @@
+from .commons_pt import *
+from src.core import register
+
+__all__ = ['YoloV9Backbone']
+
+@register
+class YoloV9Backbone(nn.Module):
+    def __init__(self, return_idx=[2,3,4]):
+        super().__init__()
+        self.return_idx = return_idx
+
+        self.pyramids = nn.Sequential(
+            nn.Sequential(
+                Silence(),
+                Conv(3, 64, 3, 2),
+            ),
+            nn.Sequential(
+                Conv(64, 128, 3, 2),
+                RepNCSPELAN4(128, 256, 128, 64)
+            ),
+            nn.Sequential(
+                ADown(256, 256, 3, 2),
+                RepNCSPELAN4(256, 512, 256, 128, 1)
+            ),            
+            nn.Sequential(
+                ADown(512, 1024, 3, 2),
+                RepNCSPELAN4(1024, 1024, 1024, 256, 1)
+            ),            
+            nn.Sequential(  
+                ADown(1024, 1024, 3, 2),
+                RepNCSPELAN4(1024, 1024, 1024, 256, 1)
+            ),  
+        )            
+        print('pyramids length:', len(self.pyramids))
+
+    def forward(self, x):
+        results = []
+        for i in range(self.return_idx[-1]+1):
+            pyr = self.pyramids[i]
+            x = pyr(x)
+            if i in self.return_idx:
+                results.append(x)
+        return results
+    
+if __name__=='__main__':
+    print(YoloV9Backbone())
